aea90aa7bfd27563fbc4ca28171de8afef47f1e3
Ousouaf2001 <ousouaf2001@gmail.com>
2016-01-11 15:13:50 +0100

    test

--- scripts/PublicationScript/PublicationsGetAllById.js
+++ scripts/PublicationScript/PublicationsGetAllById.js
@@ -32,54 +32,49 @@ $(document).ready(function(){
               $('.load_detail_publication').show();
           },
           success:function(result){
-			  $.each(result.publications, function (index, item) { 
-			  $('#publication_list').append(+
-			  	"<div class='container'>"+
-					  "<div class='pub' style='padding-bottom:10px;margin-bottom:10px;'>"+
-					  "<div class='row'>"+
-						 "<div class='col-xs-3'>"+
-							"<img src='"+item.member_photo+" 'class='imgresponsive img-circle ' width= '50' height='50'>"+
-						  "</div>"+
-						  "<div class='col-xs-6'>"+
-							"<div class='row'>"+
-								"<div class='col-xs-12'>"+
-									"<h4>"+item.member+"</h4>"+
-								"</div>"+
-								"<div class='col-xs-12'>"+
-									"<p>"+item.formatted_text+"</p>"+
-								"</div>"+
-								"<div class='col-xs-12'>");
-								
-								
-								if(item.video_link!=""){$('#publication_list').append("<button class='btn btn-sm btn_index' onclick=\"window.open('"+item.video_link+"')\"><i class='glyphicon glyphicon-facetime-video'></i>&nbsp;&nbsp;Video </button>");}
-					if(item.web_link!=""){$('#publication_list').append("<button class='btn btn-sm btn_index' onclick=\"window.open('"+item.web_link+"')\"><i class='glyphicon glyphicon-globe'></i>&nbsp;&nbsp;Link </button>");}
-					if(item.photos!=""){$('#publication_list').append("<button class='btn btn-sm  btn_index' onclick=\"window.open('"+item.photos+"')\"><i class='glyphicon glyphicon-camera'></i>&nbsp;&nbsp;Photo </button>");}
-									/*"<button class='btn btn-sm btn_index'><i class='glyphicon glyphicon-facetime-video'></i>&nbsp;&nbsp;Video</button> &nbsp;"+
-									"<button class='btn btn-sm btn_index'><i class='glyphicon glyphicon-globe'></i>&nbsp;&nbsp;Link</button>&nbsp;"+
-									"<button class='btn btn-sm  btn_index'><i class='glyphicon glyphicon-camera'></i>&nbsp;&nbsp;Photo</button>"+*/
-									
-									
-			   $('#publication_list').append(+
-								"</div>"+
-							"</div>"+
-						  "</div>"+
-						  "<div class='col-xs-3'>"+
-								"<h5>"+item.date+"</h5>"+
-						  "</div>"+
-					   "</div>"+
-					   "</div>"+
-					   "</div>"+
-			  "");
-			   
-			  });
-			  
-			  
-					/*
-					if(item.video_link!=""){$('#publication_list').append("<button class='btn btn-sm btn_index' onclick=\"window.open('"+item.video_link+"')\"><i class='glyphicon glyphicon-facetime-video'></i>&nbsp;&nbsp;Video </button>");}
-					if(item.web_link!=""){$('#publication_list').append("<button class='btn btn-sm btn_index' onclick=\"window.open('"+item.web_link+"')\"><i class='glyphicon glyphicon-globe'></i>&nbsp;&nbsp;Link </button>");}
-					if(item.photos!=""){$('#publication_list').append("<button class='btn btn-sm  btn_index' onclick=\"window.open('"+item.photos+"')\"><i class='glyphicon glyphicon-camera'></i>&nbsp;&nbsp;Photo </button>");}
-					*/	  
-							  //alert(result.event.meeting_photos[0].image);		  
+			  if(result.success==1){
+				  var displayVideo = "";
+				  var displayLink = "";
+				  var displayPhoto = "";
+				  
+				  $.each(result.publications, function (index, item) {
+					  	if(item.video_link==""){displayVideo = "style='display:none;'";}
+						if(item.web_link==""){displayLink = "style='display:none;'";}
+						if(item.photos==""){displayPhoto = "style='display:none;'";}
+						
+				  		$('#publication_list').append(+
+							"<div class='container'>"+
+							  "<div class='pub' style='padding-bottom:10px;margin-bottom:10px;'>"+
+							  "<div class='row'>"+
+								 "<div class='col-xs-3'>"+
+									"<img src='"+item.member_photo+" 'class='imgresponsive img-circle ' width= '50' height='50'>"+
+								  "</div>"+
+								  "<div class='col-xs-6'>"+
+									"<div class='row'>"+
+										"<div class='col-xs-12'>"+
+											"<h4>"+item.member+"</h4>"+
+										"</div>"+
+										"<div class='col-xs-12'>"+
+											"<p>"+item.formatted_text+"</p>"+
+										"</div>"+
+										"<div class='col-xs-12'>"+
+										   "<button class='btn btn-sm btn_index' "+displayVideo+" onclick=\"window.open('"+item.video_link+"')\"><i class='glyphicon glyphicon-facetime-video'></i>&nbsp;&nbsp;Video</button> &nbsp;"+
+											"<button class='btn btn-sm btn_index' "+displayLink+" onclick=\"window.open('"+item.web_link+"')\"><i class='glyphicon glyphicon-globe'></i>&nbsp;&nbsp;Link</button>&nbsp;"+
+											"<button class='btn btn-sm  btn_index' "+displayPhoto+" onclick=\"window.open('"+item.photos+"')\"><i class='glyphicon glyphicon-camera'></i>&nbsp;&nbsp;Photo</button>"+
+										"</div>"+
+									"</div>"+
+								  "</div>"+
+								  "<div class='col-xs-3'>"+
+										"<h5>"+item.date+"</h5>"+
+								  "</div>"+
+							   "</div>"+
+							   "</div>"+
+							   "</div>");
+				  });
+			  }else{
+				  $('#publication_list').html("Pas de Publications");
+			  }
+	  
           },
           complete: function(){
               $('.load_detail_publication').hide();
