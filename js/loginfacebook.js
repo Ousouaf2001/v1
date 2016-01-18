b153ec5694d090a924cede5622b82de8a0f996ff
lotfidev <lotfi.dev@gmail.com>
2016-01-18 12:41:37 +0100

    version facebook

--- js/loginfacebook.js
+++ js/loginfacebook.js
@@ -30,15 +30,23 @@
               path: '/me',
               success: function(data) {
                   //alert(JSON.stringify(data));
+				  sessionStorage.setItem("FacebookID",data.id);
+				  sessionStorage.setItem("name",data.first_name);
+				  sessionStorage.setItem("last_name",data.last_name);
+				  sessionStorage.setItem("ImgMember",'http://graph.facebook.com/' + data.id + '/picture?type=small');
+				  sessionStorage.setItem("email",data.email);
+				  sessionStorage.setItem("birthday",data.birthday);
+				  sessionStorage.setItem("genre",data.gender);
+				  window.location.href = 'register_fb_gplus.html';
 				  
-                  document.getElementById("FacebookID").value = data.id;
+                  /*document.getElementById("FacebookID").value = data.id;
 				  document.getElementById("name").value = data.first_name;
                   document.getElementById("last_name").value = data.last_name;
 				  document.getElementById("ImgMember").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
                   //document.getElementById("id").innerHTML = data.id;
                   document.getElementById("email").value = data.email;
                   document.getElementById("birthday").value = data.birthday;
-                  document.getElementById("genre").value = data.gender;
+                  document.getElementById("genre").value = data.gender;*/
 				  
 				  //alert("ID : "+data.id+"ID FB : "+document.getElementById("FacebookID").value);
 				  ConnectMember(1);
